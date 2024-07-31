e81e09161331c37824df0080be83e055fa840ad2
Sebastian835 <77905147+Sebastian835@users.noreply.github.com>
2024-07-30 21:07:38 -0500

    Update

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,16 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 
 let db;
 
 const connectDB = async () => {
   try {
-    console.log('Connecting to MongoDB...');
-    console.log('MONGO_URI:', process.env.MONGO_URI);
-    const client = new MongoClient(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-    });
+    console.log("Connecting to MongoDB...");
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+    const client = new MongoClient(process.env.MONGO_URI, {});
 
     await client.connect();
     db = client.db();
-    console.log('MongoDB Connected');
+    console.log("MongoDB Connected");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
